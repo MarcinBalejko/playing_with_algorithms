72d3209e746144eb0378fabee0a16b2a4d02a2cf
MarcinBalejko <mbbm9@o2.pl>
2021-02-26 23:53:29 +0100

    :white_check_mark: Created an algorithm capitalizing the first letter of each word in a string

---
+++ capitalize.js
@@ -0,0 +1,17 @@
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// Examples:
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+function capitalize(str) {
+  const strArr = str.split(" ");
+  let changedStrArr = strArr.map((el) => {
+    let changedEl = el[0].toUpperCase() + el.slice(1, el.length);
+    return changedEl;
+  });
+
+  return changedStrArr.join(" ");
+}
